a85e09fa770579f9d9554a52d2638f6ca494a498
jason <jason@ixsystems.com>
2018-11-17 04:28:08 -0800

    Added salesforce doctype

---
+++ salesforce_catcher/salesforce_catcher/doctype/salesforce_opportunity/salesforce_opportunity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, iXsystems.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SalesForce Opportunity', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ salesforce_catcher/salesforce_catcher/doctype/salesforceopportunitydetails/salesforceopportunitydetails.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, iXsystems.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SalesForceOpportunityDetails', {
+	refresh: function(frm) {
+
+	}
+});
